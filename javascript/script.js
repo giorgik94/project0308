8fa85a76ed586b30fa65113ef02fdbdd32b77b9b
Giorgi Karosanidze <giorgik94@outlook.com>
2021-07-27 11:43:14 -0700

    final cut

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,11 @@ let toggleButton = document.getElementById('burgerwrapperid');
 toggleButton.addEventListener('click', function(){
     navbarLinks.classList.toggle('activeresponsive');
 });
+
+document.getElementById('message').addEventListener('keydown', function (event){
+    if (event.which === 13) {
+        alert ('YOU PRESSED ENTER');
+    } else {
+        console.log('EVERYTHING IS OK');
+    }
+})
